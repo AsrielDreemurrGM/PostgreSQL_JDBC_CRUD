3135e5b047322cfb9b07bf665c91b02c3d53be72
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-07-05 04:37:29 -0300

    Refactored ClientDAO To Improve Code Quality
    - Replaced "SELECT *" With Explicit Column Selection;
    - Applied Try-With-Resources To Auto-Close JDBC Resources;
    - Removed Redundant Exception Catch Blocks;
    - Kept The Same Functionality, Improved Readability And Safety;
    - Added Javadoc

--- src/main/java/br/com/eaugusto/dao/ClientDAO.java
+++ src/main/java/br/com/eaugusto/dao/ClientDAO.java
@@ -8,6 +8,8 @@ import br.com.eaugusto.domain.Client;
 import br.com.eaugusto.generic.jdbc.ConnectionFactory;
 
 /**
+ * DAO implementation for Client using JDBC and PostgreSQL.
+ * 
  * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
  * @since July 5, 2025
  */
@@ -15,87 +17,49 @@ public class ClientDAO implements IClientDAO {
 
 	@Override
 	public Integer register(Client client) throws Exception {
-		Connection connection = null;
-		PreparedStatement statement = null;
+		String sql = "INSERT INTO tb_client (id, code, name) VALUES (nextval('sq_client'), ?, ?)";
 
-		try {
-			connection = ConnectionFactory.getConnection();
-			String sql = "INSERT INTO tb_client (id, code, name) VALUES (nextval('sq_client'), ?, ?)";
-			statement = connection.prepareStatement(sql);
+		try (Connection connection = ConnectionFactory.getConnection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
 
 			statement.setString(1, client.getCode());
 			statement.setString(2, client.getName());
 
 			return statement.executeUpdate();
-		} catch (Exception e) {
-			throw e;
-		} finally {
-			if (statement != null && !statement.isClosed()) {
-				statement.close();
-			}
-			if (connection != null && !connection.isClosed()) {
-				connection.close();
-			}
 		}
 	}
 
 	@Override
 	public Client search(String code) throws Exception {
-		Connection connection = null;
-		PreparedStatement statement = null;
-		ResultSet result = null;
-		Client client = null;
+		String sql = "SELECT id, code, name FROM tb_client WHERE code = ?";
 
-		try {
-			connection = ConnectionFactory.getConnection();
-			String sql = "SELECT * FROM tb_client WHERE code = ?";
-			statement = connection.prepareStatement(sql);
+		try (Connection connection = ConnectionFactory.getConnection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
 
 			statement.setString(1, code);
-
-			result = statement.executeQuery();
-
-			if (result.next()) {
-				client = new Client();
-				client.setId(result.getLong("id"));
-				client.setCode(result.getString("code"));
-				client.setName(result.getString("name"));
-			}
-			return client;
-		} catch (Exception e) {
-			throw e;
-		} finally {
-			if (statement != null && !statement.isClosed()) {
-				statement.close();
-			}
-			if (connection != null && !connection.isClosed()) {
-				connection.close();
+			try (ResultSet result = statement.executeQuery()) {
+				if (result.next()) {
+					Client client = new Client();
+					client.setId(result.getLong("id"));
+					client.setCode(result.getString("code"));
+					client.setName(result.getString("name"));
+					return client;
+				}
 			}
 		}
+		return null;
 	}
 
 	@Override
 	public Integer delete(Client client) throws Exception {
-		Connection connection = null;
-		PreparedStatement statement = null;
+		String sql = "DELETE FROM tb_client WHERE code = ?";
 
-		try {
-			connection = ConnectionFactory.getConnection();
-			String sql = "DELETE FROM tb_client WHERE code = ?";
-			statement = connection.prepareStatement(sql);
+		try (Connection connection = ConnectionFactory.getConnection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
 
 			statement.setString(1, client.getCode());
 
 			return statement.executeUpdate();
-		} catch (Exception e) {
-			throw e;
-		} finally {
-			if (statement != null && !statement.isClosed()) {
-				statement.close();
-			}
-			if (connection != null && !connection.isClosed()) {
-				connection.close();
-			}
 		}
 	}
 }
