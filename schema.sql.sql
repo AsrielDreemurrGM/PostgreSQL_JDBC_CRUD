bf2847c041f8287245f196c8c95e746321ceede2
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-07-07 14:51:27 -0300

    Updated SQL Schema With Product Table And Sequence

--- schema.sql.sql
+++ schema.sql.sql
@@ -15,3 +15,17 @@ CREATE SEQUENCE IF NOT EXISTS sq_client
     START WITH 1
     INCREMENT BY 1
     OWNED BY tb_client.id;
+
+-- Creates the product table
+CREATE TABLE IF NOT EXISTS tb_product (
+	id BIGINT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	code VARCHAR(50) NOT NULL,
+	description VARCHAR(50) NOT NULL
+);
+
+-- Creates the sequence for product IDs
+CREATE SEQUENCE IF NOT EXISTS sq_product
+    START WITH 1
+    INCREMENT BY 1
+    OWNED BY tb_product.id;
